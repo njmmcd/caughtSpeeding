3769efb2e463d29a14c020b19fda4139ae382692
Brian Painter <brianpainter@tindallcorp.com>
2015-09-07 16:38:24 -0400

    Initial Commit

---
+++ caughtSpeeding/src/caughtSpeeding/Speeding.java
@@ -0,0 +1,16 @@
+package caughtSpeeding;
+
+public class Speeding {
+	
+	public static void main(String[] args)
+	{
+		System.out.print(caughtSpeeding(60, false)); // should return 0
+		System.out.print(caughtSpeeding(65, false)); // should return 1
+		System.out.print(caughtSpeeding(65, true)); // should return 0
+	}
+	
+	public static int caughtSpeeding(int speed, boolean isBirthday)
+	{
+
+	}
+}
