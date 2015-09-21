1c3e4699336d0e694dc043de19aa494caee8cf6d
Nathan McDowell <njmmcd@hotmail.com>
2015-09-21 08:31:26 -0400

    Commit my friends.

--- caughtSpeeding/src/caughtSpeeding/Speeding.java
+++ caughtSpeeding/src/caughtSpeeding/Speeding.java
@@ -11,6 +11,34 @@ public class Speeding {
 	
 	public static int caughtSpeeding(int speed, boolean isBirthday)
 	{
-		return 0;
+
+	if (isBirthday==false) {
+		if (speed>=61 && speed<=80)
+		{return 1;}
+	
+	if (speed>=81)
+	{return 2;}
+	} else if (isBirthday=true) {
+		
+		if (speed<65) {
+			return 0;
+		}
+	
+	if (speed >=66 && speed<=85)
+	{return 1;}
+	
+	
+	if (speed >=86)
+	{return 2;}
+	
 	}
-}
+	return 0;
+	}	
+		}
+
+	
+	
+	
+
+	
+
